d5244fcbbf4a8503c5d6ba08c0a69f57ba699a28
jlopez1423 <jlopez46@ucsc.edu>
2016-09-27 19:41:00 -0700

    Day 26 of git Challenge
    Added in randomness into battleship, worked with functions(all out of a
    book I’m reading)

---
+++ dogsBaring.js
@@ -0,0 +1,14 @@
+function bark(name, weight){
+
+	if(weight > 20){
+		console.log(name + " says WOOF WOOF");
+	} else { 
+		console.log(name + " says woof woof");	
+	}
+}
+
+bark("ginger", 75);
+bark("pentunia", 10);
+
+bark("Spike", 20);
+bark("Nina", 5);
